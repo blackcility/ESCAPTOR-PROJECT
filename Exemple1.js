f1084f93e4ed6aaddb4616d65e1f0ab4f8e04565
vjblind <vjblind@users.noreply.github.com>
2019-03-18 03:06:29 +0100

    Update Exemple1.js

--- Exemple1.js
+++ Exemple1.js
@@ -5,16 +5,13 @@ super({key:"Exemple1"});
 	
 	preload(){
 	
-	this.load.image('GFS','assets/31073087_2094780174099376_3704042283549589504_n.jpg');
-	
+	 
 
 }
 
 create(){
 	
-	this.image=this.add.image(400,300,'GFS');
-	
-	
+	 
 	}
 
 
