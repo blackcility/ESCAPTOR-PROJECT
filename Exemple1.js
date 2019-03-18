e13b701f7f2474c25cf2f40f8061157a410494ae
vjblind <vjblind@users.noreply.github.com>
2019-03-18 02:42:08 +0100

    Update Exemple1.js

--- Exemple1.js
+++ Exemple1.js
@@ -1,13 +1,14 @@
-var config={
-	type :Phaser.Auto,
-	width:800,
-	height:600,
-	physics:{
-		default:'arcade',
-		arcade:{
-			gravity:{y:200}
-		}},
-	scene:[Exemple1]
-};
-var game =new PPhaser.Game(config);
+class Exemple1 extends Phaser.Scene    {
+constructor(){
+super({key:""});
+}
+	
+	preload(){}
+	
 
+}
+
+
+
+
+}
