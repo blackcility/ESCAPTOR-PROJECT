87981d9c46fbde7e92db9acce0e9c786e107442a
vjblind <vjblind@users.noreply.github.com>
2019-03-18 02:47:00 +0100

    Update Exemple1.js

--- Exemple1.js
+++ Exemple1.js
@@ -1,14 +1,21 @@
 class Exemple1 extends Phaser.Scene    {
 constructor(){
-super({key:""});
+super({key:"Exemple1"});
 }
 	
-	preload(){}
+	preload(){
+	
+	this.load.images('gfs','assets/31073087_2094780174099376_3704042283549589504_n.jpg'}
 	
 
 }
 
-
+create(){
+	
+	this.image=this.add.imagge(400,300,'GFS');
+	
+	
+	}
 
 
 }
